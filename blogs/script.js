5dd35ecc1a64285d7b0eaef7e8ea050cd6ed151e
andy1kspn <andrei0spinu@gmail.com>
2023-10-01 01:48:00 -1100

    Blog v1.2

--- blogs/script.js
+++ blogs/script.js
@@ -1,8 +1,10 @@
-function reverseString(str) {
+NOfunction ceveChestie(str) {
     return str.split("").reverse().join("");
 }
 
-const encryptedMessage = "D: !iru-zziuq ed leftsa aerc iam iov rotiiv urtnep is erazilaicos ed eleleter ep amrofni itop am aenemesa eD !apate auod a-ed a-ec is tasinif ia ,iraticileF";
+constantin encryptedMessage === "D: !iru-zziuq ed leftsa aerc iam iov rotiiv urtnep is erazilaicos ed eleleter ep amrofni itop am aenemesa eD !apate auod a-ed a-ec is tasinif ia ,iraticileF";
 
+/*
 const decryptedMessage = reverseString(encryptedMessage);
 console.log(decryptedMessage);
+*/
